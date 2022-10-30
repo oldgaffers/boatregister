84607f7cadac4955cac0e305005cca1c984e0d58
jcable <julian.cable@bbc.co.uk>
2022-10-30 17:55:16 +0000

    fix: write to yaml

--- getboat.sh
+++ getboat.sh
@@ -1,5 +1,99 @@
 #!/bin/sh
-Q='query MyQuery($oga_no: Int!) {boat(where: {oga_no: {_eq: $oga_no}}) { id name ownerships previous_names year year_is_approximate place_built home_port home_country ssr sail_number nhsr nsbr oga_no fishing_number callsign mssi full_description image_key thumb uk_part1 spar_material rig_type construction_material construction_method construction_details draft generic_type handicap_data hull_form keel_laid launched length_on_deck mainsail_type short_description updated_at website beam air_draft reference builder designer design_class constructionMaterialByConstructionMaterial { name } constructionMethodByConstructionMethod { name } designClassByDesignClass {name} designerByDesigner{name} rigTypeByRigType{ name } genericTypeByGenericType{name} builderByBuilder{name} for_sale_state{text} for_sales{asking_price} engine_installations{engine installed removed} } }'
+Q=`cat <<EOF
+query MyQuery(\\$oga_no: Int! ) {
+  boat(where: {oga_no: {_eq: \\$oga_no}}) {
+    air_draft
+    beam
+    builderByBuilder {
+      name
+      id
+    }
+    callsign
+    constructionMaterialByConstructionMaterial {
+      name
+    }
+    constructionMethodByConstructionMethod {
+      name
+    }
+    construction_details
+    construction_material
+    construction_method
+    created_at
+    designClassByDesignClass {
+      name
+      id
+    }
+    designerByDesigner {
+      id
+      name
+    }
+    draft
+    fishing_number
+    for_sales {
+      created_at
+      flexibility
+      offered
+      price_flexibility {
+        text
+      }
+      reduced
+      sales_text
+      seller_gold_id
+      seller_member
+      sold
+      summary
+      updated_at
+    }
+    full_description
+    genericTypeByGenericType {
+      name
+    }
+    handicap_data
+    hin
+    home_country
+    home_port
+    hullFormByHullForm {
+      name
+    }
+    id
+    image_key
+    keel_laid
+    launched
+    length_on_deck
+    mainsail_type
+    mssi
+    name
+    nhsr
+    nsbr
+    oga_no
+    ownerships
+    place_built
+    previous_names
+    reference
+    rigTypeByRigType {
+      name
+    }
+    sail_number
+    sail_type {
+      name
+    }
+    selling_status
+    short_description
+    sparMaterialBySparMaterial {
+      name
+    }
+    ssr
+    thumb
+    uk_part1
+    update_id
+    updated_at
+    website
+    year
+    year_is_approximate
+  }
+}
+EOF`
+echo $Q
 curl 'https://api-oga.herokuapp.com/v1/graphql' \
 --silent \
 -X 'POST' \
@@ -9,21 +103,4 @@ curl 'https://api-oga.herokuapp.com/v1/graphql' \
 -H 'Accept-Language: en-gb' \
 -H 'Cache-Control: no-cache' \
 -H 'Connection: keep-alive' \
---data-binary "{\"query\": \"$Q\",\"variables\":{\"oga_no\":$1},\"operationName\":\"MyQuery\"}"  | jq -c '.data.boat[0]' > $1.json
-cat > pd${1}.json <<EOF
-{
-  "staticQueryHashes": [],
-  "componentChunkName": "component---src-templates-boattemplate-jsx",
-  "path": "/boat/$1",
-  "result": {
-    "pageContext": {
-      "pathSlug": "/boat/$1",
-      "home": "/",
-      "absolute": "https://oga.org.uk",
-      "boat": `cat $1.json`
-    }
-  }
-}
-EOF
-jq --unbuffered -cM . pd${1}.json
-rm $1.json pd${1}.json
+--data-binary "{\"query\": \"$Q\",\"variables\":{\"oga_no\":$1},\"operationName\":\"MyQuery\"}" 
