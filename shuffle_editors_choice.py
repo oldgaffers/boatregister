d2f3635b5ee48f6ae1db376e2c8ab1c8ee9173c8
jcable <julian.cable@bbc.co.uk>
2022-12-03 14:23:43 +0000

    feat: support featuring a boat

--- shuffle_editors_choice.py
+++ shuffle_editors_choice.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from os import listdir
 from datetime import date, datetime
 import requests
@@ -11,12 +12,7 @@ def json_serial(obj):
         return obj.isoformat(timespec='seconds')[:-6]+'Z'
     raise TypeError ("Type %s not serializable" % type(obj))
 
-if __name__ == '__main__':
-    f = open('fleets/editors choice')
-    editors_choice = json.load(f)
-    f.close()
-    mypath='boat/'
-    boats = listdir(mypath)
+def shuffle(boats):
     with_pictures = []
     without_pictures = []
     for b in boats:
@@ -31,6 +27,19 @@ if __name__ == '__main__':
             without_pictures.append(oga_no)
     random.shuffle(with_pictures)
     random.shuffle(without_pictures)
-    editors_choice['filters']['oga_nos'] = with_pictures + without_pictures
+    return with_pictures + without_pictures
+
+if __name__ == '__main__':
+    f = open('fleets/editors choice')
+    editors_choice = json.load(f)
+    f.close()
+    if len(sys.argv) > 1:
+        featured_oga_no = int(sys.argv[1])
+        boats = editors_choice['filters']['oga_nos']
+        b = [boat for boat in boats if boat != featured_oga_no]
+        editors_choice['filters']['oga_nos'] = [featured_oga_no] + b
+    else:
+        boats = listdir('boat/')
+        editors_choice['filters']['oga_nos'] = shuffle(boats)
     with open('fleets/editors choice', "w") as stream:
         json.dump(editors_choice, stream, ensure_ascii=False, default=json_serial)
