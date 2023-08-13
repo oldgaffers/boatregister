3e465707cc2a6d348ad69bef5bcdf21b681fa220
jcable <julian.cable@bbc.co.uk>
2023-08-13 18:05:21 +0100

    fix: sail

--- make_filterable.py
+++ make_filterable.py
@@ -32,8 +32,7 @@ def wanted(boat):
     'length_on_deck',
     'price',
     'sale',
-    'for_hire',
-    'crewing_opportunities'
+    'sail',
     ]
     if 'selling_status' in boat and boat['selling_status'] == 'for_sale':
       boat['price'] = boat['for_sales'][0]['asking_price']
