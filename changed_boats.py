1ba246c6977fe77b7faa224065ecde053cc061a9
jcable <julian.cable@bbc.co.uk>
2023-12-10 12:01:29 +0000

    fix: wip

--- changed_boats.py
+++ changed_boats.py
@@ -3,8 +3,12 @@ import json
 
 boats = []
 files = environ['CHANGED_FILES']
+print(f'CHANGED_BOATS_INPUT={files}')
 for file in files.split(' '):
   if file.startswith('boat'):
+    print(f'CHANGED_BOATS_FILE_{file}=wanted')
     oga_no = int(file.split('/')[1])
     boats.append(oga_no)
+  else:
+    print(f'CHANGED_BOATS_FILE_{file}=not wanted')
 print(f'CHANGED_BOATS={json.dumps(boats)}')
