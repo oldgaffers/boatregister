45087a3080df72e4f926316f64172c4a5f9798ec
jcable <julian.cable@bbc.co.uk>
2024-01-11 22:46:34 +0000

    fix: pass on fail

--- changed_boats.py
+++ changed_boats.py
@@ -5,10 +5,13 @@ boats = []
 files = environ['CHANGED_FILES']
 print(f'CHANGED_BOATS_INPUT={files}')
 for file in files.split(' '):
-  if file.startswith('boat'):
-    print(f'CHANGED_BOATS_FILE_{file}=wanted')
-    oga_no = int(file.split('/')[1])
-    boats.append(oga_no)
-  else:
-    print(f'CHANGED_BOATS_FILE_{file}=not wanted')
+  try:
+    if file.startswith('boat'):
+      print(f'CHANGED_BOATS_FILE_{file}=wanted')
+      oga_no = int(file.split('/')[1])
+      boats.append(oga_no)
+    else:
+      print(f'CHANGED_BOATS_FILE_{file}=not wanted')
+  except:
+    pass
 print(f'BOATS={json.dumps(boats)}')
