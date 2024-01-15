6d0c380f34154bd86e4191ff5ff7283045f3af82
jcable <julian.cable@bbc.co.uk>
2024-01-15 19:48:00 +0000

    add script

---
+++ boat_field.py
@@ -0,0 +1,24 @@
+import yaml
+import sys
+
+def get_boat(path):
+  with open(path, "r") as stream:
+    try: 
+      boat = yaml.safe_load(stream)
+    except Exception as e:
+      print(e)
+      print('OGA', path)
+      boat = None
+  if boat is None:
+    return None
+  else:
+    return boat
+
+if __name__ == '__main__':
+  oga_no = sys.argv[1]
+  field = sys.argv[2]
+  try:
+    boat = get_boat(f"boat/{oga_no}/boat.yml")
+    print(f'{field}={boat[field]}')
+  except:
+    print(f'{field}=?')
