4a07e9c67ad4bee7d67461212048be575bd35a8c
jcable <boatregister@oga.org.uk>
2025-09-20 17:02:32 +0100

    layout

--- places.py
+++ places.py
@@ -6,10 +6,14 @@ from make_filterable import get_boat, json_serial
 if __name__ == '__main__':
   mypath = 'boat'
   boats = listdir(mypath)
-  data = set()
+  data = {}
   for b in boats:
     boat = get_boat(f"{mypath}/{b}/boat.yml")
     if boat and 'place_built' in boat:
-      data.add(boat['place_built'])
+      place = boat['place_built'].strip()
+      if data.get(place):
+        data[place] += 1
+      else:
+        data[place] = 1
   with open("places.json", "w") as stream:
       json.dump(sorted(list(data)), stream, ensure_ascii=False, default=json_serial)
