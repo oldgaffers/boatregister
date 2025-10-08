99fbc7c4f77f59f6c68832e413070c43350301eb
jcable <boatregister@oga.org.uk>
2025-10-08 13:43:44 +0100

    fix: implementation

--- update_member_ownerships.py
+++ update_member_ownerships.py
@@ -2,6 +2,7 @@
 import sys
 import yaml
 from os import listdir
+from datetime import datetime
 from helpers import dump
 
 def get_boat(path):
@@ -13,27 +14,75 @@ def get_boat(path):
     print('OGA', path)
   return None
 
-def update_owner(boat, action, firstname, lastname, id, member):
-  updated = {**boat}
+def update_owner(boat, status, firstname, lastname, id, member):
   dirty = False
-  if not dirty:
-    return None
-  return updated
+  ownerships = boat.get('ownerships', [])
+  name = f"{firstname} {lastname}".title()
+  if status == 'Member':
+    a = []
+    b = []
+    for os in ownerships:
+      if os.get('name', None) == name:
+        a.append(os)
+        dirty = True
+      else:
+        b.append(os)
+    for os in a:
+      print(boat['oga_no'], os)
+      del os['name']
+      os['id'] = id
+      os['member'] = member
+    ownerships = [*a, *b]
+  else:
+    a = []
+    b = []
+    for os in ownerships:
+      if os.get('id', None) == id:
+        a.append(os)
+        dirty = True
+      else:
+        b.append(os)
+    for os in a:
+      os['name'] = name
+      del os['id']
+      del os['member']
+      if status == 'Non-member':
+        pass
+      elif status == 'Deceased':
+        if boat.get('current', False):
+          del boat['current']
+        if 'end' not in boat:
+          os['end'] = datetime.now().year
+      else:
+        print('unknown status', status, firstname, lastname, id, member)
+    ownerships = [*a, *b]
+  if dirty:
+    print('updated', boat['name'], boat['oga_no'])
+    return { **boat, 'ownerships': ownerships }
+  return None
+
+def usage():
+  print('usage: update_member_ownerships Member|Non-member|Deceased firstname lastname gold_id membership_number')
 
 if __name__ == '__main__':
-  action = sys.argv[1]
-  status = sys.argv[2]
-  firstname = sys.argv[3]
-  lastname = sys.argv[4]
-  id = sys.argv[5]
-  member = sys.argv[6]
+  if len(sys.argv) != 6:
+    usage()
+    exit()
+  status = sys.argv[1]
+  firstname = sys.argv[2]
+  lastname = sys.argv[3]
+  id = int(sys.argv[4])
+  member = int(sys.argv[5])
+  if status not in ['Member', 'Non-member', 'Deceased']:
+    usage()
+    exit()
   mypath='boat'
   boats = listdir(mypath)
   for b in boats:
     path = f"{mypath}/{b}/boat.yml"
     boat = get_boat(path)
     if boat is not None:
-      updated = update_owner(boat, action, firstname, lastname, id, member)
+      updated = update_owner(boat, status, firstname, lastname, id, member)
       if updated is not None:
         with open(path, "w") as stream:
           dump(updated, stream)
