b4cef1e4b3939ebd5005a297114661126519c167
Julian Cable <julian.cable@yahoo.com>
2025-11-04 17:21:27 +0000

    Update merge_field.py

--- merge_field.py
+++ merge_field.py
@@ -8,21 +8,21 @@ from os import listdir
 from pathlib import Path
 from helpers import dump
 
-def replace(field, old, new):
-  without = [f for f in field if f['name'] == 'old']
+def replace(field, merge, new):
+  without = [f for f in field if f['name'] not in merge]
   if len(without) == len(field):
     return field # old not present
   without.append(new)
   return without
 
-def merge_boats(field, old, new):
+def merge_boats(field, merge, new):
   boats = listdir('boat') # ?
   for b in boats:
     boat = None
     p = f"boat/{b}/boat.yml"
     with open(p, "r", encoding='utf-8') as stream:
       boat = yaml.safe_load(stream)
-    boat[field] = replace(boat.get('field', []), old, new)
+    boat[field] = replace(boat.get('field', []), merge, new)
     with open(p, 'w') as outfile:
       dump(boat, outfile)
 
@@ -31,9 +31,8 @@ def merge_field(field, keep, merge):
     pickers = json.load(stream)
   pl = pickers[field]
   new = [p for p in pl if p['name'] == keep][0]
-  for old in merge:
-    print(f'replace {old} with {keep} as {field} for all boats')
-    merge_boats(field, old, new)
+  print(f'replace {', '.join(merge)} with {keep} as {field} for all boats')
+  merge_boats(field, merge, new)
   print(f'remove {merge} from {field} picklist')
   pl2 = [p for p in pl if p['name'] not in merge]
   # save pl2 as yaml
