c44918208aebc61156c70e8ef1ec73e442f6e8bb
Julian Cable <julian.cable@yahoo.com>
2026-05-20 08:07:50 +0100

    Update changed_boats.py

--- changed_boats.py
+++ changed_boats.py
@@ -6,9 +6,12 @@ files = ' '.join(list(fileinput.input()))
 print(f'CHANGED_BOATS_INPUT={files}')
 for file in files.split(' '):
   try:
-    if file.startswith('boat'):
+    if '/boat/' in file:
       print(f'CHANGED_BOATS_FILE_{file}=wanted')
-      oga_no = int(file.split('/')[1])
+      p = file.split('/')
+      n = 1 if p[0] == 'boat'
+      n = 2 if p[1] == 'boat'
+      oga_no = int(p[n])
       boats.append(oga_no)
     else:
       print(f'CHANGED_BOATS_FILE_{file}=not wanted')
