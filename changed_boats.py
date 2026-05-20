e6ba957d6a0c9990511d817f5b6f5f44ad5af7d0
Julian Cable <julian.cable@yahoo.com>
2026-05-20 08:40:58 +0100

    Update changed_boats.py

--- changed_boats.py
+++ changed_boats.py
@@ -1,19 +1,20 @@
+import system
 import json
 
 boats = []
 files = input()
-print(f'CHANGED_BOATS_INPUT={files}')
+print(f'CHANGED_BOATS_INPUT={files}', file=sys.stderr)
 for file in files.split(' '):
   try:
     if '/boat/' in file:
-      print(f'CHANGED_BOATS_FILE_{file}=wanted')
+      print(f'CHANGED_BOATS_FILE_{file}=wanted', file=sys.stderr)
       p = file.split('/')
       n = 1 if p[0] == 'boat' else 2
       n = 2 if p[1] == 'boat' else 1
       oga_no = int(p[n])
       boats.append(oga_no)
     else:
-      print(f'CHANGED_BOATS_FILE_{file}=not wanted')
+      print(f'CHANGED_BOATS_FILE_{file}=not wanted', file=sys.stderr)
   except:
     pass
 print(f'BOATS={json.dumps(boats)}')
