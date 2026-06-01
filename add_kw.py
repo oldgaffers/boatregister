989a4eac4ffdbbc659291ca3dfab1e4d2121867f
Julian Cable <julian.cable@yahoo.com>
2026-06-01 15:26:44 +0000

    add script to add keywords

---
+++ add_kw.py
@@ -0,0 +1,126 @@
+import json
+import sys
+import os
+import yaml
+import requests
+from requests_oauthlib import OAuth1Session
+
+cs = os.environ.get('SMUGMUG_CLIENT_SECRET', 'n/a')
+sac = os.environ.get('SMUGMUG_SECRET_ACCESS_KEY', 'n/a')
+ac = os.environ.get('SMUGMUG_ACCESS_KEY', 'n/a')
+api_key = os.environ.get('SMUGMUG_API_KEY', 'n/a')
+
+def getRequestsHandler():
+    return OAuth1Session(api_key,
+        client_secret=cs,
+        resource_owner_key=ac,
+        resource_owner_secret=sac
+    )
+
+def get_album(oga_no):
+    text = f'({oga_no})'
+    r = requests.get(f'https://api.smugmug.com/api/v2/album!search',
+        headers={'accept': 'application/json' },
+        params={
+            'APIKey': api_key,
+            'Scope': '/api/v2/user/oga',
+            'SortDirection': 'Descending',
+            'SortMethod': 'Rank',
+            'Text': text,
+        }
+    )
+    if r.ok:
+        js = r.json()
+        r = js['Response']['Album']
+        for a in r:
+          if a['UrlName'] == f'OGA-{oga_no}':
+             return a
+    else:
+        print(f'Error fetching gallery name for {oga_no}: {r.status_code}')
+    return None
+
+def boat(no):
+    with open(f"boat/{no}/boat.yml", "r", encoding='utf-8') as stream:
+      boat = yaml.safe_load(stream)
+    return boat
+
+def update_kw(url, kw):
+    smugmug = getRequestsHandler()
+    r = smugmug.patch(f'https://api.smugmug.com{url}',
+        headers={'accept': 'application/json', 'Content-Type': 'application/json' },
+        json={
+            'KeywordArray': kw,
+        }
+    )
+    if not r.ok:
+        print(f'Error updating image keywords: {r.status_code} {r.text}')
+        return
+
+def update_all(images, kw):
+    for image in images:
+        ekw = image['KeywordArray']
+        m = list(set(ekw + kw))
+        if m != ekw:
+            update_kw(image["Uri"], m)
+
+def add_to_all(images, kw):
+    urls = []
+    for image in images:
+        r = requests.get(f"https://api.smugmug.com{image['Uri']}",
+            headers={'accept': 'application/json' },
+            params={'APIKey': api_key}
+        )
+        if r.ok:
+            js = r.json()
+            urls.append(js['Response']['AlbumImage']['Uris']['Image']['Uri'])
+    print(urls)
+    smugmug = getRequestsHandler()
+    r = smugmug.post(f'https://api.smugmug.com/api/v2/image!addkeywords',
+        headers={'accept': 'application/json', 'Content-Type': 'application/json' },
+        json={
+            'Async': True,
+            'ImageUris': urls,
+            'Keywords': ';'.join(kw),
+        }
+    )
+    if not r.ok:
+        print(f'Error updating image keywords: {r.status_code} {r.text}')
+        return
+
+def add_kw(no):
+    print(f'OGA No, {no}!')
+    album = get_album(no)
+    if album is None:
+        print(f"OGA No {no} has no gallery")
+        return
+    imagesUri = album['Uris']['AlbumImages']['Uri']
+    url = f'https://api.smugmug.com{imagesUri}'
+    print(url)
+    r = requests.get(url,
+        headers={'accept': 'application/json' },
+        params={'APIKey': api_key}
+    )
+    if r.ok:
+        js = r.json()
+        images = js['Response']['AlbumImage']
+    else:
+        print(f'Error fetching gallery name for {no}: {r.status_code}')
+        return
+    print(len(images))
+    b = boat(no)
+    kw = [b['name']]
+    gt = b.get('generic_type', [])
+    if type(gt) == str:
+       gt = [gt]
+    dc = b.get('design_class', None)
+    if dc is not None:
+       kw.append(dc)
+    kw = kw + gt
+    print(kw)
+    add_to_all(images, kw)
+
+if __name__ == '__main__':
+  if len(sys.argv) == 2:
+    boats = json.loads(sys.argv[1])
+    for n in boats:
+        add_kw(n)
