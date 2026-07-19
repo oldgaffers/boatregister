f0acbd7fc9dcfa930fa4dd00c92829990b39fc73
Julian Cable <julian.cable@yahoo.com>
2026-07-19 21:53:01 +0100

    Update make_editors_choice.py

--- make_editors_choice.py
+++ make_editors_choice.py
@@ -25,6 +25,15 @@ def json_serial(obj):
     return obj.isoformat(timespec='seconds')[:-6]+'Z'
   raise TypeError ("Type %s not serializable" % type(obj))
 
+def has_pictures(oga_no):
+  r = requests.get(f'https://oga.smugmug.com/Boats/OGA-{oga_no}/')
+  if not r.ok:
+    return False
+  t = r.text()
+  if 'No photos' in t:
+    return False 
+  return True
+
 if __name__ == '__main__':
   f = open('fleets/editors choice')
   editors_choice = json.load(f)
@@ -37,8 +46,7 @@ if __name__ == '__main__':
     if boat is not None:
       oga_no = int(b)
       if oga_no not in chosen:
-        r = requests.head(f'https://oga.smugmug.com/Boats/OGA-{oga_no}/')
-        if r.ok:
+        if has_pictures(oga_no):
             chosen.insert(0, oga_no)
         else:
             chosen.append(oga_no)
